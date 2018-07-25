e115df63532f68695683afa6ed7d13f51b1fc769
Mehdi Baha <el-mehdi.baha@worldline.com>
2018-07-25 10:57:50 +0200

    Update syncing repo feature

--- sync_projects.py
+++ sync_projects.py
@@ -1,33 +1,49 @@
 import bs4
 import requests
-import ruamel.yaml
-import sys
+import yaml
 
+from collections import defaultdict
+
+# config yaml w/ default dict
+from yaml.representer import Representer
+yaml.add_representer(defaultdict, Representer.represent_dict)
+
+# Constants
 GITHUB_USERNAME = 'mehdibaha'
 DEF_ICON = 'ga ga-github'
+CATEGORIES = {
+    '\U0001F468': 'Side projects',
+    '\U0001F4BC': 'Work',
+    '\U0001F4D1': 'Documentation',
+    '\U0001F3EB': 'School',
+}
 
-url = f'https://github.com/{GITHUB_USERNAME}'
-html = requests.get(url).text
-soup = bs4.BeautifulSoup(html, 'html.parser')
+def gen_projects():
+    url = f'https://github.com/{GITHUB_USERNAME}?tab=repositories'
+    html = requests.get(url).text
+    soup = bs4.BeautifulSoup(html, 'html.parser')
+    sources = defaultdict(list)
+    for repo in soup.find_all('li', {'itemprop': 'owns'}):
+        tag = repo.find('a')
+        name = tag.text.strip()
+        description = repo.find('p').text.strip()
+        # generate url
+        url = tag.text[1:].strip()
+        url = f'{GITHUB_USERNAME}/{url}' if '/' not in url else url
+        url = f'https://github.com/{url}'
+        # check if contains emoji at beginning
+        if any(key in description for key in CATEGORIES.keys()):
+            # retrieve category of repo based on emoji
+            category = CATEGORIES.get(description[0]) 
+            sources[category].append({'name': name, 'description': description, 'url': url})
+    projects = {'name': 'Projects', 'source': sources}
+    return projects
 
-projects = {'name': 'Projects'}
-sources = []
+def main():
+    projects = gen_projects()
+    yaml.dump(projects, stream=open('data/projects.yml', 'w'), default_flow_style=False)
 
-for repo in soup.find_all('li', {'class': 'pinned-repo-item'}):
-	tag = repo.find_all('a', {'class': 'text-bold'})[0]
-	# finding name
-	name = tag.find_all('span', {'class': 'repo'})[0].text
-	name = name.capitalize()
-	# finding description
-	description = repo.find_all('p', {'class': 'pinned-repo-desc'})[0].text
-	description = f'{description[1:]}'
-	# finding url
-	url = tag.text[1:-1]
-	url = f'{GITHUB_USERNAME}/{url}' if '/' not in url else url
-	url = f'https://github.com/{url}'
-	# adding complete repo
-	sources.append({'name': name, 'description': description, 'url': url, 'icon': DEF_ICON})
-projects['source'] = sources
+if __name__ == '__main__':
+    main()
 
-yaml = ruamel.yaml.YAML()
-yaml.dump(projects, open('data/projects.yml', 'w'))
+main()
