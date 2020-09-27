8acaadbd61921ca4dac2c65c0e34db37700c827c
Mehdi Baha <elmehdi.baha@gmail.com>
2020-09-27 19:20:41 +0000

    Update projects section

--- sync_projects.py
+++ sync_projects.py
@@ -1,5 +1,4 @@
 import bs4
-import re
 import requests
 import yaml
 
@@ -14,22 +13,9 @@ GITHUB_USERNAME = 'mehdibaha'
 DEF_ICON = 'ga ga-github'
 CATEGORIES = {
     '\U0001F468': '\U0001F468\u200D\U0001F4BB Side projects',
-    '\U0001F4BC': '\U0001F4BC Work',
-    '\U0001F4D1': '\U0001F4D1 Documentation',
+    '\U0001F4BC': '\U0001F4BC Interview assignments',
     '\U0001F3EB': '\U0001F3EB School',
 }
-
-# https://stackoverflow.com/questions/33404752
-def remove_emoji(string):
-    emoji_pattern = re.compile("["
-                           u"\U0001F600-\U0001F64F"  # emoticons
-                           u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-                           u"\U0001F680-\U0001F6FF"  # transport & map symbols
-                           u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-                           u"\U00002702-\U000027B0"
-                           u"\U000024C2-\U0001F251"
-                           "]+", flags=re.UNICODE)
-    return emoji_pattern.sub(r'', string)
 
 def gen_projects():
     url = f'https://github.com/{GITHUB_USERNAME}?tab=repositories'
@@ -48,7 +34,7 @@ def gen_projects():
         if any(key in description for key in CATEGORIES.keys()):
             # retrieve category of repo based on emoji
             category = CATEGORIES.get(description[0])
-            description = remove_emoji(description).strip("\u200D").strip()
+            description = description.encode('latin-1', 'ignore').decode('latin-1').strip()
             sources[category].append({'name': name, 'description': description, 'url': url})
     projects = {'name': 'Projects', 'source': sources}
     return projects
